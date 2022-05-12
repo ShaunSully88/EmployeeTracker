3a21e6d673f9f2c87d550572a2427e483f9859c4
Shaun Sullivan <shaunsully88@gmail.com>
2022-05-12 13:38:27 -0400

    seed and schema successfully running. id's are sycning correctly too, all is good

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 
-USE employees
+USE employees;
 
 CREATE TABLE department (
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -13,7 +13,7 @@ id INTEGER AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) UNIQUE NOT NULL,
 salary DECIMAL NOT NULL,
 department_id INTEGER NOT NULL,
-INDEX dep_ind (department_id)
+INDEX dep_ind (department_id),
 CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,41 @@
 INSERT INTO department (name)
 VALUES
+('Forward'),
+('Defense'),
+('Goalie'),
+('Brass');
+
 
 INSERT INTO role (title, salary, department_id)
 VALUES
+('Center', 2000000, 1),
+('Left Wing', 1500000, 1),
+('Right Wing', 1500000, 1),
+('Left Defense', 1000000, 2),
+('Right Defense', 1000000, 2),
+('Starter', 2000000, 3),
+('Backup', 800000, 3),
+('Head Coach', 500000, 4),
+('General Manager', 750000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
+('Sidney', 'Crosby', 1, 1),
+('Evgeni', 'Malkin', 1, 2),
+('Jeff', 'Carter', 1, NULL),
+('Jake', 'Guentzel', 2, NULL),
+('Jason', 'Zucker', 2, NULL),
+('Danton', 'Heinen', 2, NULL),
+('Bryan', 'Rust', 3, NULL),
+('Kasperi', 'Kapanen', 3, NULL),
+('Brock', 'McGinn', 3, NULL),
+('Brian', 'Dumolin', 4, 3),
+('Mike', 'Matheson', 4, NULL),
+('Kris', 'Letang', 5, 4),
+('John', 'Marino', 5, NULL),
+('Tristan', 'Jarry', 6, NULL),
+('Casey', 'DeSmith', 7, NULL),
+('Louis', 'Domingue', 7, NULL),
+('Mike', 'Sullivan', 8, 5),
+('Ron', 'Hextall', 9, 6),
+(' Brian', 'Burke', 9, 7);
